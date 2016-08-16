00bc7f1dac256666b02953afdb84419c34e3ff76
nickby <nick.mail@tut.by>
2016-08-16 20:30:58 +0300

    Set project structure

---
+++ database.php
@@ -0,0 +1,2 @@
+<?php
+    echo "database connect<br>";
---
+++ models/articles.php
@@ -0,0 +1,22 @@
+<?php
+
+function articles_all(){
+    echo "show all articles<br>";
+    return true;
+}
+
+function articles_get($id){
+    
+}
+
+function articles_new($title, $date, $content){
+    
+}
+
+function articles_edit($id, $title, $date, $content){
+    
+}
+
+function articles_delete($id){
+    
+}
